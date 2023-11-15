538d6ef921c79fbfca731d1fb4b609811cc0501b
Afikilenkonyane <afikly.nkonyane@gmail.com>
2023-11-15 02:43:00 -0800

    updated

---
+++ pid.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - print pid
+ *
+ * Return: ALWAYS  0
+ */
+int main(void)
+{
+    pid_t my_pid;
+
+    my_pid = getpid();
+    printf("%u\n", my_pid);
+    return (0);
+}
