0ae791d07674e77ce5cb7a740e989af50b39dbc3
Afikilenkonyane <afikly.nkonyane@gmail.com>
2023-11-16 14:28:29 -0800

    updated

---
+++ _node1.c
@@ -0,0 +1,100 @@
+#include "shell.h"
+
+/**
+ * list_len - linked ls
+ * @h: first NODE
+ *
+ * Return: ls
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * get_node_index - get NODE
+ * @head: ls HEAD
+ * @node: NODE
+ *
+ * Return: index of NODE or else -1
+ */
+ssize_t get_node_index(list_t *head, list_t *node)
+{
+	size_t i = 0;
+
+	while (head)
+	{
+		if (head == node)
+			return (i);
+		head = head->next;
+		i++;
+	}
+	return (-1);
+}
+
+/**
+ * node_starts_with - returns NODE
+ * @node: ls HEAD
+ * @prefix: str
+ * @c: char after prefix
+ *
+ * Return: match NODE or NULL
+ */
+list_t *node_starts_with(list_t *node, char *prefix, char c)
+{
+	char *p = NULL;
+
+	while (node)
+	{
+		p = starts_with(node->str, prefix);
+		if (p && ((c == -1) || (*p == c)))
+			return (node);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * list_to_strings - Strings list to str
+ * @head: First node
+ *
+ * Return: array of str
+ */
+char **list_to_strings(list_t *head)
+{
+	list_t *node = head;
+	size_t i = list_len(head), j;
+	char **strs;
+	char *str;
+
+	if (!head || !i)
+		return (NULL);
+	strs = malloc(sizeof(char *) * (i + 1));
+	if (!strs)
+		return (NULL);
+	for (i = 0; node; node = node->next, i++)
+	{
+		str = malloc(_strlen(node->str) + 1);
+		if (!str)
+		{
+			for (j = 0; j < i; j++)
+				free(strs[j]);
+			free(strs);
+			return (NULL);
+		}
+
+		str = _strcpy(str, node->str);
+		strs[i] = str;
+	}
+	strs[i] = NULL;
+	return (strs);
+}
+
+
--- pid.c
+++ pid.c
@@ -5,12 +5,12 @@
  * main - print pid
  *
  * Return: ALWAYS  0
- */
+*/
 int main(void)
 {
-    pid_t my_pid;
+	pid_t my_pid;
 
-    my_pid = getpid();
-    printf("%u\n", my_pid);
-    return (0);
+	my_pid = getpid();
+	printf("%u\n", my_pid);
+	return (0);
 }
